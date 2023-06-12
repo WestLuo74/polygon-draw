2d83a78d47203aa6ade1e7856cc4aaeb6ac7bf39
LuoYuhuan <west_luo@163.com>
2023-06-12 15:13:06 +0800

    实现检查线段交叉和错误提示

--- src/components/PolygonDraw.vue
+++ src/components/PolygonDraw.vue
@@ -19,7 +19,7 @@
           @mousedown="handleCanvasMouseDown"
           @contextmenu="handleContextMenu"
         ></canvas> <!-- @dblclick="handleDbClick" -->
-        
+
         <!--存储已生成的点线，避免被清空-->
         <!-- <canvas
           ref="canvasSave"
@@ -29,11 +29,78 @@
           @mousedown="handleCanvasMouseDown"
         ></canvas> -->
       </div>
+      <!-- <div v-if="alertType" :class="alertType" class="alert alert-box" role="alert">{{ alertMsg }}</div> -->
+      <el-dialog v-if="alertType" :visible="true">
+        <el-alert title="" center :type="alertType" show-icon> {{ alertMsg }}  </el-alert>
+      </el-dialog>
     </div>
   </template>
    
   <script>
-  // import FlvCop from "@/viewsCommon/components/FlvCop.vue";
+  //判断两个线段是否相交
+  function isIntersection(a, b, c, d) {
+    /** 1 解线性方程组, 求线段交点. **/
+    // 如果分母为0 则平行或共线, 不相交
+    var denominator = (b.y - a.y) * (d.x - c.x) - (a.x - b.x) * (c.y - d.y);
+    if (denominator == 0) {
+      return false;
+    }
+
+    // 线段所在直线的交点坐标 (x , y)
+    var x =
+      ((b.x - a.x) * (d.x - c.x) * (c.y - a.y) +
+        (b.y - a.y) * (d.x - c.x) * a.x -
+        (d.y - c.y) * (b.x - a.x) * c.x) /
+      denominator;
+    var y =
+      -(
+        (b.y - a.y) * (d.y - c.y) * (c.x - a.x) +
+        (b.x - a.x) * (d.y - c.y) * a.y -
+        (d.x - c.x) * (b.y - a.y) * c.y
+      ) / denominator;
+
+    /** 2 判断交点是否在两条线段上 **/
+    if (
+      // 交点在线段1上
+      (x - a.x) * (x - b.x) < 0 &&
+      (y - a.y) * (y - b.y) < 0 &&
+      // 且交点也在线段2上
+      (x - c.x) * (x - d.x) < 0 &&
+      (y - c.y) * (y - d.y) < 0
+    ) {
+      // // 返回交点p
+      // return {
+      //   x: x,
+      //   y: y,
+      // };
+      return true;
+    }
+    //否则不相交
+    return false;
+  }
+
+  function isExistIntersection(points) {
+    let result = false;
+    for (let i = 0; i < points.length - 1; i++) {
+      let a = points[i];
+      let b = points[i + 1];
+      for (let j = 0; j < points.length - 1; j++) {
+        if (i == j) {
+          continue;
+        }
+        let c = points[j];
+        let d = points[j + 1];
+        result = isIntersection(a, b, c, d);
+        if (result) {
+          return result;
+        }
+        // console.log(result)
+      }
+    }
+    return result;
+  }
+
+    // import FlvCop from "@/viewsCommon/components/FlvCop.vue";
   export default {
     name: "PolygonDraw",
     data() {
@@ -49,9 +116,14 @@
         color: 'black', //新建的area的color
         points: [], //新建的area的点
         lastPoint: {}, //新建的area的最后一个点
+
+        alertType: null,
+        alertMsg: '',
+        alertTime: null
       };
     },
-    mounted(){
+    mounted() {
+      this.setTimer()
       this.initCanvas()
       this.refresh()
     },
@@ -99,7 +171,7 @@
             ctx.lineTo(pointArr[i].x, pointArr[i].y);
           }
           ctx.strokeStyle = area.color;
-          ctx.fillStyle = "rgba(161,195,255,0.5)"; //填充颜色
+          ctx.fillStyle = "rgba(161,195,255,0.5)"; //填充颜色，半透明
           ctx.closePath();
           ctx.fill();
           ctx.stroke(); //绘制
@@ -119,17 +191,21 @@
       refresh(){
         // this.ctx.clearRect(0, 0, this.ctx.width, this.ctx.height);
         this.clearCanvas(this.can)
+        
         for(let area of this.areas){
           this.drawArea(this.ctx, area)
         }
-        if(this.points.length > 0){
-          let points = [].concat(this.points)
-          points.push(this.lastPoint)
-          this.drawArea(this.ctx, {
-            title: this.title,
-            color: this.color,
-            points: points
-          })
+
+        if (this.mode === 'create'){
+          if(this.points.length > 0){
+            let points = [].concat(this.points)
+            points.push(this.lastPoint)
+            this.drawArea(this.ctx, {
+              title: this.title,
+              color: this.color,
+              points: points
+            })
+          }
         }
       },
       initCanvas() {
@@ -155,29 +231,32 @@
         // console.log(can);
         // console.log(canSave);
       },
-      create(title, color){
+      create(title='area 1', color='black'){
         this.title = title
         this.color = color
         this.mode = 'create'
       },
-      createFinish(){
-        if(this.mode !== 'create'){
+      createFinish() {
+        if (this.mode !== 'create') {
           return
         }
         this.mode = ''
-        if(this.points.length < 3){ //多边形顶点不能小于3个
-          console.log('多边形顶点不能小于3个')
-          return
+        if (this.points.length < 3) { //多边形顶点不能小于3个
+          this.showError('多边形顶点不能小于3个')          
+        }else if (isExistIntersection(this.points)) {
+          this.showError("存在交叉线段，请重新绘制");
+        }else{
+          this.areas.push({ //加入到areas
+            title: this.title,
+            color: this.color,
+            points: this.points
+          })
         }
-        this.areas.push({ //加入到areas
-          title: this.title,
-          color: this.color,
-          points: this.points
-        })
+
         this.title = ''
         this.points = []
       },
-      handleCanvasMouseUp() {
+        handleCanvasMouseUp() {
        
       },
       handleCanvasMouseDown(e) {
@@ -218,8 +297,40 @@
       handleContextMenu(e){
         e.preventDefault();
         return false;
-      }
-    }
+      },
+      showError: function (msg) {
+        this.showAlert("error", msg);
+        // alert(msg);
+      },
+      showSuccess: function (msg) {
+        //alert(msg);
+        this.showAlert("success", msg);
+      },
+      showAlert: function (type, msg) { //为防止显示后导致右键菜单隐藏失效，延时100ms再显示
+        setTimeout(() => {
+          this.alertType = type;
+          this.alertMsg = msg;
+          this.alertTime = new Date();
+          // alert(msg);
+        }, 200)
+      },
+      setTimer() {
+        if (this.timer == null) {
+          this.timer = setInterval(() => {
+            if (!this.alertTime) {
+              return
+            }
+            // console.log('开始定时...每过一秒执行一次')
+            let now = new Date();
+            //显示信息，3秒后消失
+            if (parseInt(now - this.alertTime) > 3000) {
+              this.alertType = "";
+              this.alertMsg = "";
+            }
+          }, 1000)
+        }
+      },
+    },
   };
   </script>
    
