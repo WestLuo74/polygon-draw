7d84b1e19160994c696f8e44cd8332d14566621b
LuoYuhuan <west_luo@163.com>
2023-06-16 13:50:50 +0800

    删除main.js中无用的bus

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-import Bus from 'vue';
-
-Vue.prototype.$Bus = new Bus();
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
